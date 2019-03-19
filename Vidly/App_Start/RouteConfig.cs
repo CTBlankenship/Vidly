9cd65488d4f181d45f9ae5bcc0523efc39d34242
CTBlankenship <chthbl@protonmail.com>
2019-03-19 16:22:41 -0400

    13. Attribute Routing ... could not get to work

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -11,13 +11,15 @@ namespace Vidly
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.MapMvcAttributeRoutes();
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
                     "MoviesByReleaseDate",
                     "movies/released/{year}/{month}",
-                    new {controller="Movies",action="ByReleaseDate"},
-                    new {year = @"2015|2016", month = @"\d{2}"}
+                    new { controller = "Movies", action = "ByReleaseDate" },
+                    new { year = @"2015|2016", month = @"\d{2}" }
 
             );
 
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -35,6 +35,7 @@ namespace Vidly.Controllers
 
         }
 
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year, byte month)
         {
             return Content(year + "/" + month);
