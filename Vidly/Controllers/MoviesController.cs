086b2681f32b85c2f8c345261568af5d9a06bbf6
CTBlankenship <chthbl@protonmail.com>
2019-03-19 15:54:20 -0400

    11. Action Parameters

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,22 +1,39 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
-        public ViewResult Index()
-        {
-            return View();
-        }
+        //GET: Movies
+        //public ViewResult Index()
+        //{
+        //    return View();
+        //}
 
-        public ActionResult Random()
+        public ViewResult Random()
         {
             var movie = new Movie() {Name = "Shrek!"};
 
 
             return View(movie);
+        }
+
+        public ActionResult Edit(int movieId)
+        {
+            return Content("Id =" + movieId);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+
         }
     }
 }
